b5d24a9b292e3b4801bead8c2c91002c9ef4c685
juandazeng <juanda@gmail.com>
2024-02-19 22:49:13 +0800

    Various enhancements

--- generate-image-os-list.py
+++ generate-image-os-list.py
@@ -4,14 +4,22 @@ import json
 import ssl
 import re
 from urllib.request import urlopen, Request
-from pathlib import Path
+from datetime import datetime
 
 # Constants
 UBI_IDENTIFIER_IN_LABEL = "\"url\"=\"https://access.redhat.com/containers/#/registry.access.redhat.com/ubi"
 OS_ID_RHEL = "rhel"
-UBI_REGEX = r"(https\://.+/([^/]+)/images/(\d[\d.]*)\-(\d[\d.]*))"
-CSV_HEADER = ["Cluster", "Namespace", "Deployment", "Image", "OS", "OS Image Name", "OS Image Version", "OS Image Release"]
-IS_EXCLUDE_OPENSHIFT_NAMESPACE = True
+UBI_PREFIX = "ubi"
+UBI_REGEX = r"(?:https\://.+/([^/]+)/images/(\d[\d.]*)\-(\d[\d.]*))"
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S %z"
+CSV_HEADER = ["Cluster Name", "Environment", "Cluster Descriptor", "Namespace", "Deployment", "Image", "Created On", "OS", "UBI Name", "UBI Version", "UBI Release"]
+IS_INCLUDE_OPENSHIFT_NAMESPACE = False
+
+# The cluster regex matches the following:
+# ocps4 - uat_abc_def123 <-- cluster name: ocps4, environment: uat, cluster descriptor: abc_def123
+# ocps4 - uat            <-- cluster name: ocps4, environment: uat
+# ocps4                  <-- cluster name: ocps4
+CLUSTER_INFO_REGEX = r"([^\W_]+)(?:[\W_]+([^\W_]+)(?:[\W_]+(.*))?)?$"
 
 # Prepare for API calls
 rhacsApiUrl = None
@@ -55,79 +63,128 @@ def main():
             # Process all deployments across all clusters
             deployments = responseJson["deployments"]
 
-            # Skip all openshift namespaces is IS_EXCLUDE_OPENSHIFT_NAMESPACE is True
-            deploymentsToBeInspected = [deployment for deployment in deployments if not deployment["namespace"].startswith("openshift")] if IS_EXCLUDE_OPENSHIFT_NAMESPACE else deployments
+            # Skip all openshift namespaces unless IS_INCLUDE_OPENSHIFT_NAMESPACE is True
+            deploymentsToBeInspected = deployments if IS_INCLUDE_OPENSHIFT_NAMESPACE else [deployment for deployment in deployments if not deployment["namespace"].startswith("openshift")] 
 
             deploymentCount = len(deploymentsToBeInspected)
             currentDeploymentIndex = 0
             for deployment in deploymentsToBeInspected:
-                cluster = deployment["cluster"]
+                clusterName = deployment["cluster"]
+                clusterEnvironment = ""
+                clusterDescriptor = ""
+
+                # Try to parse cluster info
+                try:
+                    regexResult = re.search(CLUSTER_INFO_REGEX, clusterName)
+                    if regexResult.group(1) is not None:
+                        clusterName = regexResult.group(1)
+                    if regexResult.group(2) is not None:
+                        clusterEnvironment = regexResult.group(2)
+                    if regexResult.group(3) is not None:
+                        clusterDescriptor = regexResult.group(3)
+                except:
+                    pass
+
                 namespace = deployment["namespace"]
                 deploymentId = deployment["id"]
                 deploymentName = deployment["name"]
 
                 # Get the deployment detail
                 currentDeploymentIndex += 1
-                print(f"{currentDeploymentIndex} of {deploymentCount} - Inspecting {cluster}/{namespace}/{deploymentName}...")
-                responseJson = getJsonFromRhacsApi("/deployments/" + deploymentId)
-                if responseJson is not None:
-                    containers = responseJson["containers"]
-                    for container in containers:
-                        imageId = container["image"]["id"]
-                        
-                        # Get the image detail
-                        responseJson = getJsonFromRhacsApi("/images/" + imageId)
-                        if responseJson is not None:
-                            imageFullName = responseJson["name"]["fullName"]
-                            os = responseJson["scan"]["operatingSystem"]
+                print(f"{currentDeploymentIndex} of {deploymentCount} - Inspecting {clusterName}/{namespace}/{deploymentName}...")
+
+                try:
+                    responseJson = getJsonFromRhacsApi("/deployments/" + deploymentId)
+                    if responseJson is not None:
+                        containers = responseJson["containers"]
+                        for container in containers:
+                            image = container["image"]
+                            imageId = image["id"]
+                            imageFullName = image["name"]["fullName"]
+                            
+                            # Get the image detail
+                            createdOn = ""
+                            os = ""
                             ubiName = ""
                             ubiVersion = ""
                             ubiRelease = ""
+                            try:
+                                print(f"{imageFullName}")
+                                responseJson = getJsonFromRhacsApi("/images/" + imageId)
+                                if responseJson is not None:
+                                    metadataJson = responseJson["metadata"]["v1"]
+                                    createdOn = metadataJson["created"]
 
-                            # Get more details if it's a rhel-based image
-                            if os.startswith(OS_ID_RHEL):
-                                for layer in responseJson["metadata"]["v1"]["layers"]:
-                                    if layer["instruction"] == "LABEL":
-                                        value = layer["value"]
-                                        # UBI-specific metadata checking
-                                        if UBI_IDENTIFIER_IN_LABEL in value:
-                                            regexResult = re.search(UBI_REGEX, value)
-                                            if regexResult is not None:
-                                                ubiName = regexResult.group(2)
-                                                ubiVersion = regexResult.group(3)
-                                                ubiRelease = regexResult.group(4)
-                                                # Exit the current loop
-                                                break
+                                    # Try to convert createdOn from ISO format to a more readable format
+                                    try:
+                                        createdOn = datetime.fromisoformat(createdOn).astimezone().strftime(DATETIME_FORMAT)
+                                    except:
+                                        pass
 
-                                # If the base image labels have been removed,
-                                # try to get the metadata from the url
-                                if ubiName == "":
-                                    labels = responseJson["metadata"]["v1"]["labels"]
-                                    if hasattr(labels, "url"):
-                                        regexResult = re.search(UBI_REGEX, labels["url"])
-                                        if regexResult is not None:
-                                            ubiName = regexResult.group(2)
-                                            ubiVersion = regexResult.group(3)
-                                            ubiRelease = regexResult.group(4)
-                                    
-                                    # If that failed, try to get the metadata from the labels
-                                    if ubiName == "":
-                                        ubiName = labels["name"]
-                                        ubiVersion = labels["version"]
-                                        ubiRelease = labels["release"]
+                                    os = responseJson["scan"]["operatingSystem"]
 
-                            # Write the image detail into the CSV file
-                            writer.writerow([
-                                cluster,
-                                namespace,
-                                deploymentName,
-                                imageFullName,
-                                os,
-                                ubiName,
-                                ubiVersion,
-                                ubiRelease
-                            ])
-                            f.flush()
+                                    # Get more details if it's a rhel-based image
+                                    if os.startswith(OS_ID_RHEL):
+                                        for layer in metadataJson["layers"]:
+                                            if layer["instruction"] == "LABEL":
+                                                value = layer["value"]
+                                                # UBI-specific metadata checking
+                                                if UBI_IDENTIFIER_IN_LABEL in value:
+                                                    regexResult = re.search(UBI_REGEX, value)
+                                                    if regexResult is not None:
+                                                        ubiName = regexResult.group(1)
+                                                        ubiVersion = regexResult.group(2)
+                                                        ubiRelease = regexResult.group(3)
+                                                        # Exit the current loop
+                                                        break
+
+                                        # If the base image labels have been removed,
+                                        # try to get the metadata from the url
+                                        if ubiName == "":
+                                            labels = metadataJson["labels"]
+                                            if hasattr(labels, "url"):
+                                                regexResult = re.search(UBI_REGEX, labels["url"])
+                                                if regexResult is not None:
+                                                    ubiName = regexResult.group(1)
+                                                    ubiVersion = regexResult.group(2)
+                                                    ubiRelease = regexResult.group(3)
+                                            
+                                            # If that failed, try to get the metadata from the labels
+                                            if ubiName == "":
+                                                ubiName = labels["name"]
+                                                ubiVersion = labels["version"]
+                                                ubiRelease = labels["release"]
+
+                                        # Ignore non-ubi metadata
+                                        if not ubiName.startswith(UBI_PREFIX):
+                                            ubiName = ""
+                                            ubiVersion = ""
+                                            ubiRelease = ""
+
+                            except Exception as ex:
+                                os = type(ex)
+                                ubiName = ex
+                                print(f"Image:{imageFullName} has the following ERROR:{type(ex)=}:{ex=}.")
+
+                            finally:
+                                # Write the image detail into the CSV file
+                                writer.writerow([
+                                    clusterName,
+                                    clusterEnvironment,
+                                    clusterDescriptor,
+                                    namespace,
+                                    deploymentName,
+                                    imageFullName,
+                                    createdOn,
+                                    os,
+                                    ubiName,
+                                    ubiVersion,
+                                    ubiRelease
+                                ])
+                                f.flush()
+
+                except Exception as ex:
+                    print(f"Not completing {clusterName}/{namespace}/{deploymentName} due to ERROR:{type(ex)=}:{ex=}.")
 
         print(f"Successfully generated {csvFileName}\n")
                     
